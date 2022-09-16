8ac4e76c8dd1d1579c513e9157fb9563990b76ea
User-P <pether.1994@gmail.com>
2022-09-16 17:53:52 -0500

    bases

---
+++ src/app/contador/contador.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { ContadorComponent } from './contador/contador.componet';
+
+@NgModule({
+  declarations: [ContadorComponent],
+  imports: [],
+  exports: [ContadorComponent],
+})
+export class ContadorModule {}
